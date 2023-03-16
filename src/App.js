3ed0e369227def52d9349c80250668e6bb052757
Toby <bayonleadebisijose@gmail.com>
2023-03-16 18:53:56 +0100

    remodification of homepage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Token from "./container/token/Token";
 import Homepage from "./container/homepage/Homepage";
-import Services from "./container/services/Services";
-import Roadmap from "./container/roadmap/Roadmap";
-import Social from "./container/social/Social";
-import Footer from "./components/footer/Footer";
 import ContactUs from "./container/contact/ContactUs";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+        <Navbar />
       <Routes>
-        <Route path="/" element= { <Homepage />} />
-        <Route path="/services" element= { <Services />} />
-        <Route path="token" element= { <Token />} />
-        <Route path="/roadmap" element= { <Roadmap />} />
-        <Route path="/social" element= { <Social />} />
-        <Route path="/footer" element= { <Footer />} />
-        <Route path="/contact" element= { <ContactUs />} />
-        {/* <Homepage />
-        <Services />
-        <Token />
-        <Roadmap />
-        <Social />
-        <Footer /> */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        
       </Routes>
     </div>
   );
--- src/container/homepage/Homepage.jsx
+++ src/container/homepage/Homepage.jsx
@@ -4,33 +4,48 @@ import twitterLogo from "../../assets/icons/twitter.svg";
 import telegramLogo from "../../assets/icons/telegram.svg";
 import gitHubLogo from "../../assets/icons/github.svg";
 import dataChart from "../../assets/images/data-chart.png";
+import Services from "../services/Services";
+import Token from "../token/Token";
+import Roadmap from "../roadmap/Roadmap";
+import Social from "../social/Social";
+import Footer from "../../components/footer/Footer";
 
 const Homepage = () => {
   return (
-    <section className="hero__section">
-      <p>Bringing Change To Blockchain Gaming Forever </p>
-      <div className="hero__section-btn">
-        <button className="game-btn">Games</button>
-        <button className="token-btn">Buy tokens</button>
-      </div>
-      <div className="image__icon">
-        <img src={twitterLogo} alt="Twitter-icon" />
-        <img src={gitHubLogo} alt="Github-icon" />
-        <img src={telegramLogo} alt="Telegram-icon" />
-      </div>
-
-       <section className="objective__section">
-        <div className="objective__heading">
-          <h2>Our Mission</h2>
-          <p className="objective__text">
-            Combining these three major key elements, which are vital for our platform.Give power to the community by having their voices heard while we, at Solidus, provide the tools: an immersive Gaming environment powered by Blockchain technology.
-          </p>
-        </div> 
-        <div className="objective__image">
-          <img src={dataChart} alt="Data Presentation" />
+    <>
+      <section className="hero__section">
+        <p>Bringing Change To Blockchain Gaming Forever </p>
+        <div className="hero__section-btn">
+          <button className="game-btn">Games</button>
+          <button className="token-btn">Buy tokens</button>
         </div>
+        <div className="image__icon">
+          <img src={twitterLogo} alt="Twitter-icon" />
+          <img src={gitHubLogo} alt="Github-icon" />
+          <img src={telegramLogo} alt="Telegram-icon" />
+        </div>
+
+        <section className="objective__section">
+          <div className="objective__heading">
+            <h2>Our Mission</h2>
+            <p className="objective__text">
+              Combining these three major key elements, which are vital for our
+              platform.Give power to the community by having their voices heard
+              while we, at Solidus, provide the tools: an immersive Gaming
+              environment powered by Blockchain technology.
+            </p>
+          </div>
+          <div className="objective__image">
+            <img src={dataChart} alt="Data Presentation" />
+          </div>
       </section>
-    </section>
+        </section>
+        <Services />
+        <Token />
+        <Roadmap />
+        <Social />
+        <Footer />
+    </>
   );
 };
 
